8ae716d718ce61971c38a16c66febbe268139358
JulioSteve <jules.stevenot25@gmail.com>
2025-12-08 15:28:49 +0100

    Energies - need to recheck leapfrog/forces computation (can optimize force sby 3rd law of Newton)

--- Nbody/Nbody.py
+++ Nbody/Nbody.py
@@ -3,79 +3,85 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 
 # Load data
+Tsteps, N, mod = np.loadtxt("settings.dat", unpack=True, skiprows=1, usecols=(0,1,3), dtype=int)
+dt = np.loadtxt("settings.dat", unpack=True, skiprows=1, usecols=(2), dtype=np.float32)
 
-timesteps = len(t) // N  # number of frames
+t = np.array([x*dt for x in range(0,Tsteps+1, mod)]) #We tested the reconstruction to ensure it is the right formula
 
-# Setup figure
-plt.rc('figure', facecolor="black")
-plt.rc('axes', facecolor='black', edgecolor='red', labelcolor='red')
-plt.rc('grid', color='grey')
-plt.rc('text', color='green')
+POS = np.fromfile("pos.bin", dtype=np.float32).reshape((len(t),N,3), order="F")
+VEL = np.fromfile("vel.bin", dtype=np.float32).reshape((len(t),N,3), order="F")
 
-f = plt.figure()
-ax = f.add_subplot(projection='3d')
 
-# Initial scatter
-scatp = dict(s=100, c='gold', marker="*")
-scat = ax.scatter(x[:N], y[:N], z[:N], **scatp)
+# # Setup figure
+# plt.rc('figure', facecolor="black")
+# plt.rc('axes', facecolor='black', edgecolor='red', labelcolor='red')
+# plt.rc('grid', color='grey')
+# plt.rc('text', color='green')
 
-# Animation update function
-def update(frame):
-    start = frame * N
-    end = start + N
-    X = x[start:end]
-    Y = y[start:end]
-    Z = z[start:end]
-    # print(f"Frame {frame}: X = {X}, Y = {Y}, Z = {Z}")  # Debugging
-    progress = (frame+1)/timesteps*100
-    print(f"\rGenerating animation... {progress:5.1f}%", end='')
-    scat._offsets3d = (X, Y, Z)
-    return scat,
+# f = plt.figure()
+# ax = f.add_subplot(projection='3d')
 
-# Aesthetics
-ax.xaxis.set_pane_color((0, 0, 0, 1))
-ax.yaxis.set_pane_color((0, 0, 0, 1))
-ax.zaxis.set_pane_color((0, 0, 0, 1))
+# # Initial scatter
+# scatp = dict(s=100, c='gold', marker="*")
+# scat = ax.scatter(x[:N], y[:N], z[:N], **scatp)
 
-# Working frame
-boxlim = (-1,1)
-ax.set(xlim=boxlim,ylim=boxlim,zlim=boxlim)
+# # Animation update function
+# def update(frame):
+#     start = frame * N
+#     end = start + N
+#     X = x[start:end]
+#     Y = y[start:end]
+#     Z = z[start:end]
+#     # print(f"Frame {frame}: X = {X}, Y = {Y}, Z = {Z}")  # Debugging
+#     progress = (frame+1)/timesteps*100
+#     print(f"\rGenerating animation... {progress:5.1f}%", end='')
+#     scat._offsets3d = (X, Y, Z)
+#     return scat,
 
-ax.tick_params(colors='white')
+# # Aesthetics
+# ax.xaxis.set_pane_color((0, 0, 0, 1))
+# ax.yaxis.set_pane_color((0, 0, 0, 1))
+# ax.zaxis.set_pane_color((0, 0, 0, 1))
 
-# Create animation (store reference in a variable to avoid garbage collection)
-freq = 60 #FPS (Hz) 
-tau = 1000/freq#interval in ms between images of animation.
-# ani = anim.FuncAnimation(fig=f, func=update, frames=timesteps, interval=tau, blit=False)
+# # Working frame
+# boxlim = (-1,1)
+# ax.set(xlim=boxlim,ylim=boxlim,zlim=boxlim)
 
-# Generating a mp4 video of the simulation | or | showing the animation in live to user.
-animtitle = f"N{N}_T5e3_dt5e-4_NOeps_60fps"
-# ani.save(animtitle+".gif", writer="pillow",fps=freq)
-# ani.save(animtitle+".avi", writer="ffmpeg", bitrate=5000,fps=freq)
-# plt.show()
+# ax.tick_params(colors='white')
 
-# Generating the plot of the trajectories, fixed on the center of mass:
-# t2, x2, y2, z2 = np.loadtxt("2body.dat", skiprows=1,unpack=True, usecols=[0,1,2,3])
-# def traj2(T,X,Y,Z):
-#     pos1 = []
-#     pos2 = []
-#     for i in range(len(T)):
-#         if i%2==0:
-#             pos1.append([X[i],Y[i],Z[i]])
-#             print(pos1)
-#         else:
-#             pos2.append([X[i],Y[i],Z[i]])
+# # Create animation (store reference in a variable to avoid garbage collection)
+# freq = 60 #FPS (Hz) 
+# tau = 1000/freq#interval in ms between images of animation.
+# # ani = anim.FuncAnimation(fig=f, func=update, frames=timesteps, interval=tau, blit=False)
+
+# # Generating a mp4 video of the simulation | or | showing the animation in live to user.
+# animtitle = f"N{N}_T5e3_dt5e-4_NOeps_60fps"
+# # ani.save(animtitle+".gif", writer="pillow",fps=freq)
+# # ani.save(animtitle+".avi", writer="ffmpeg", bitrate=5000,fps=freq)
+# # plt.show()
+
+# # Generating the plot of the trajectories, fixed on the center of mass:
+# # t2, x2, y2, z2 = np.loadtxt("2body.dat", skiprows=1,unpack=True, usecols=[0,1,2,3])
+# # def traj2(T,X,Y,Z):
+# #     pos1 = []
+# #     pos2 = []
+# #     for i in range(len(T)):
+# #         if i%2==0:
+# #             pos1.append([X[i],Y[i],Z[i]])
+# #             print(pos1)
+# #         else:
+# #             pos2.append([X[i],Y[i],Z[i]])
     
-#     TRAJ = (pos1[0],pos1[1],pos2[0],pos2[1])
-#     CM = np.mean(TRAJ)
-#     return CM
+# #     TRAJ = (pos1[0],pos1[1],pos2[0],pos2[1])
+# #     CM = np.mean(TRAJ)
+# #     return CM
 
-# CM = traj2(t2, x2, y2, z2)
+# # CM = traj2(t2, x2, y2, z2)
 
-# print(CM)
-
-
+# # print(CM)
 
 
 
-print("") #Just so the percentage in animation doesn't ruin the terminal layout.
+
+
+# print("") #Just so the percentage in animation doesn't ruin the terminal layout.
--- Nbody/Nbody_eps.f90
+++ Nbody/Nbody_eps.f90
@@ -1,94 +1,123 @@
-program main
-    implicit none
-    integer :: i,j
+    program main
+        implicit none
+        integer :: i,j
 
-    integer,parameter :: Tsteps=int(1e3), N=int(1e3)
-    real, parameter :: dt=5e-3, m=1./N, mtot=m*N, omega=1., G=1., R=1.
+        integer,parameter :: Tsteps=int(1e3), N=int(1e3), modu=10
+        real, parameter :: dt=5e-3, m=1./N, mtot=m*N, omega=1., G=1., R=1., eps=0.99
 
-    real :: x,y,z,dist0, t
-    real,dimension(N,3) :: pos,vel
-    real, dimension(3) :: vcm
-    integer,dimension(8) :: seed
-    
-    seed = [324549107,1517762999,2043478039,169521159,-1935932375,513922432,1075905891,-1147996143]
-    call random_seed(put=seed)
-    
-    do i=1,N
-        dist0 = R
-        do while (dist0>=R)
-            call random_number(x)
-            call random_number(y)
-            call random_number(z)
-            x = 2*x-1
-            y = 2*y-1
-            z = 2*z-1
-            
-            dist0 = sqrt(x*x+y*y+z*z)
-        end do
-        pos(i,1) = x
-        pos(i,2) = y
-        pos(i,3) = z
-        vel(i,1) = -y*omega
-        vel(i,2) = x*omega
-        vel(i,3) = 0
-    end do
-    
-    do j=1,3
-        vcm(j) = sum(vel(:,j))/N
-        vel(:,j) = vel(:,j)-vcm(j)
-    enddo
+        real :: x,y,z,dist0, t, Ep, Ek
 
-    t = 0.
-    open(unit=10,file="settings.dat")
-    write(10,*) "Time steps, number of bodies, dt"
-    write(10,*) Tsteps, N, dt
-    close(10)
-
-    open(unit=11,file="pos.bin",form="unformatted")
-    open(unit=12,file="vel.bin",form="unformatted")
-    do j=0,Tsteps
-        if (mod(j,10)==0) then
-            write(11) pos(:,:)
-            write(12) vel(:,:)
-        endif
-        call leapfrog(dt,pos,vel)
-        t = t+dt
-    enddo
-    close(11)
-    close(12)
-
-    contains
-        subroutine force(f,pos1,pos2)
-            real,dimension(3),intent(in) :: pos1,pos2
-            real,dimension(3),intent(out) :: f
-            real :: fscal, eps
-            eps=0.99
-            
-            fscal = (sqrt((pos1(1)-pos2(1))**2+&
-            (pos1(2)-pos2(2))**2+(pos1(3)-pos2(3))**2+eps*eps)**3)
-
-            fscal = G*m*m/fscal
-
-            f = fscal*(pos2-pos1)
-        end subroutine force
+        real,dimension(N,3) :: pos,vel
+        real, dimension(3) :: vcm
+        integer,dimension(8) :: seed
         
-        subroutine leapfrog(dt,r,v)
-            real,intent(in) :: dt
-            real,dimension(N,3),intent(inout) :: r,v
-            real,dimension(3) :: f
-            integer :: i,j
-
-            do i=1,N-1
-                f = 0
-                do j=i+1,N
-                    call force(f,r(i,:),r(j,:))
-
-                    v(i,:) = v(i,:) + dt/m*f
-                    v(j,:) = v(j,:) - dt/m*f
-                enddo
+        seed = [324549107,1517762999,2043478039,169521159,-1935932375,513922432,1075905891,-1147996143]
+        call random_seed(put=seed)
+        
+        do i=1,N
+            dist0 = R
+            do while (dist0>=R)
+                call random_number(x)
+                call random_number(y)
+                call random_number(z)
+                x = 2*x-1
+                y = 2*y-1
+                z = 2*z-1
                 
-                r(i,:) = r(i,:) + v(i,:)*dt
-            enddo
-            r(N,:) = r(N,:) + v(N,:)*dt
-        end subroutine leapfrog
-end program main
+                dist0 = sqrt(x*x+y*y+z*z)
+            end do
+            pos(i,1) = x
+            pos(i,2) = y
+            pos(i,3) = z
+            vel(i,1) = -y*omega
+            vel(i,2) = x*omega
+            vel(i,3) = 0
+        end do
+        
+        do j=1,3
+            vcm(j) = sum(vel(:,j))/N
+            vel(:,j) = vel(:,j)-vcm(j)
+        enddo
+
+        t = 0.
+        open(unit=10,file="settings.dat")
+        write(10,*) "Time steps, number of bodies, dt, modulo (every simulation results are printed at each modulo)"
+        write(10,*) Tsteps, N, dt, modu
+        close(10)
+
+        open(unit=11,file="pos.bin",form="unformatted", access="stream", status="replace")
+        open(unit=12,file="vel.bin",form="unformatted", access="stream", status="replace")
+        open(unit=13, file="energies.bin", form="unformatted", access="stream", status="replace")
+        do j=0,Tsteps
+            if (mod(j,modu)==0) then
+                Ek = 0.5*m*sum(vel(:,1)*vel(:,1)+vel(:,2)*vel(:,2)+vel(:,3)*vel(:,3))
+                Ep = potential(pos(:,:))
+
+                write(11) pos(:,:)
+                write(12) vel(:,:)
+                write(13) Ek, Ep
+            endif
+            call leapfrog(dt,pos,vel)
+            t = t+dt
+        enddo
+        close(11)
+        close(12)
+        close(13)
+
+        contains
+            subroutine force(f,pos1,pos2)
+                real,dimension(3),intent(in) :: pos1,pos2
+                real,dimension(3),intent(out) :: f
+                real :: fscal
+                
+                fscal = (sqrt((pos1(1)-pos2(1))**2+&
+                (pos1(2)-pos2(2))**2+(pos1(3)-pos2(3))**2+eps*eps)**3)
+
+                fscal = G*m*m/fscal
+
+                f = fscal*(pos2-pos1)
+            end subroutine force
+            
+            subroutine leapfrog(dt,r,v)
+                real,intent(in) :: dt
+                real,dimension(N,3),intent(inout) :: r,v
+                real,dimension(3) :: f
+                integer :: i,j
+
+                do i=1,N-1
+                    f = 0
+                    do j=i+1,N
+                        call force(f,r(i,:),r(j,:))
+
+                        v(i,:) = v(i,:) + dt/m*f
+                        v(j,:) = v(j,:) - dt/m*f
+                    enddo
+                    
+                    r(i,:) = r(i,:) + v(i,:)*dt
+                enddo
+                r(N,:) = r(N,:) + v(N,:)*dt
+            end subroutine leapfrog
+
+            function potential(pos)
+                real,dimension(N,3),intent(in) :: pos
+                real, intent(out) :: potential
+                integer :: i,j
+                real :: dx,dy,dz
+
+                potential = 0
+                do i=1,N
+                    do j=1,N
+                        if j>i then
+                            dx = pos(j,1)-pos(i,1)
+                            dy = pos(j,2)-pos(i,2)
+                            dz = pos(j,3)-pos(i,3)
+
+                            potential = potential + 1./sqrt(dx*dx+dy*dy+dz*dz+eps*eps)
+                        endif
+                    enddo
+                enddo
+
+                potential = -G*m*m*P
+                
+            end function potential
+    end program main
