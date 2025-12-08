bdc4614e7487d102bbff887bc16fe3dd2b76b721
JulioSteve <jules.stevenot25@gmail.com>
2025-12-08 16:05:21 +0100

    Re-arrangement of the files from exercices

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FILE="ex5_red"
-for thread in $(seq 1 1 16)
-do
-    export OMP_NUM_THREADS=$thread
-    time ./$FILE
-done
--- test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-DAT = np.fromfile("data.bin", dtype=float)
-
-print(DAT)
